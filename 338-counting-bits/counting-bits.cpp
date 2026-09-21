d8825de318fc3479fb3179aee5c5820db64ae857
Gungun <2k24.csai1b.2411918@gmail.com>
2026-09-21 11:36:15 +0530

    Time: 2 ms (35.21%) | Memory: 11.3 MB (18.47%) - LeetSync

---
+++ 338-counting-bits/counting-bits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>ans;
+        ans.push_back(0);
+       for(int i=1;i<=n;i++){
+        int count=0;
+        int num=i;
+         while(num!=0){
+         num=num&(num-1);
+           count++;
+         }
+         ans.push_back(count);
+
+       }
+       return ans; 
+    }
+};
+// this approach uses tc=0(n logn);
